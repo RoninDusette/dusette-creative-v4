dbb00992e19357dc1862a207922e85dba8673561
Ronin Dusette <ronindusette@Ronins-iMac.local>
2018-08-09 10:27:51 -0700

    fixes

--- DusetteCreativeV4/djgui/src/js/components/Home/HomeLoader.jsx
+++ DusetteCreativeV4/djgui/src/js/components/Home/HomeLoader.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 export const HomeLoader = () => (
   <div id={"loader-wrapper"}>
     <div className={"loader-img-wrapper text-center"}>
-      <img src={"/static/frontend/img/icons/logo-plain.png"} className={"loader-img"} alt=""/>
+      <img src={"/img/icons/logo-plain.png"} className={"loader-img"} alt=""/>
       <h3 id={"loader-text"}><span id={"loader-text-word"}>Loading</span><span id={"loader-text-dot-1"}>.</span><span
         id={"loader-text-dot-2"}>.</span><span id={"loader-text-dot-3"}>.</span></h3>
     </div>
--- DusetteCreativeV4/urls.py
+++ DusetteCreativeV4/urls.py
@@ -20,5 +20,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'', include('DusetteCreativeV4.modules.frontend.urls')),
+    re_path('.*', include('DusetteCreativeV4.modules.frontend.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
